159767ce0fabd2b61438504cf27562e7824f692d
techcore <sudarshanparjane777@gmail.com>
2025-10-22 11:29:18 -0400

    Delete agricare-app directory

--- agricare-app/finalyear_main/agricare-app/app/static/js/app.js
+++
@@ -1,5 +0,0 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/static/service-worker.js")
-    .then(() => console.log("Service Worker registered"))
-    .catch(err => console.error("SW failed:", err));
-}
